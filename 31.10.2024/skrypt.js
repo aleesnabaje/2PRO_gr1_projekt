94ca4defe18f317db262c499ed347b05ab7763c6
aleesnabaje <nimasno13@gmail.com>
2024-10-31 20:08:09 +0100

    gotowe zadanie

--- 31.10.2024/skrypt.js
+++ 31.10.2024/skrypt.js
@@ -1,9 +1,10 @@
-let bdy = document.querySelector("body")
+const bdy = document.querySelector("body");
 
-const h1 = document.createElement("h1")
-h1.textContent = "Zgadnij stolice!";
+const h1 = document.createElement("h1");
+h1.textContent = "Zgadnij stolicę!";
+bdy.appendChild(h1);
 
-const div = document.createElement("div")
+const div = document.createElement("div");
 div.style.width = "400px";
 div.style.height = "375px";
 div.style.border = "1px solid black";
@@ -11,124 +12,111 @@ div.style.backgroundColor = "lightgray";
 div.style.textAlign = "center";
 div.style.fontFamily = "calibri";
 div.style.margin = "auto";
+bdy.appendChild(div);
 
-const divpkt = document.createElement("div")
+const divpkt = document.createElement("div");
 divpkt.style.width = "300px";
 divpkt.style.height = "100px";
-divpkt.style.border = "1px solid black"
-divpkt.style.margin = "auto"
+divpkt.style.border = "1px solid black";
+divpkt.style.margin = "auto";
+bdy.appendChild(divpkt);
 
-let punktpop = 0
-let punktnie = -1
+let punktpop = 0;
+let punktnie = 0;
 
-let pktacja = document.createElement("ul")
+const pktacja = document.createElement("ul");
 pktacja.textContent = "Punktacja";
 
-let pop = document.createElement("li")
-pop.textContent = "Poprawne: "+punktpop;
+const pop = document.createElement("li");
+pop.textContent = "Poprawne: " + punktpop;
 
-let niepop = document.createElement("li")
+const niepop = document.createElement("li");
+niepop.textContent = "Niepoprawne: " + punktnie;
 
+pktacja.appendChild(pop);
+pktacja.appendChild(niepop);
+divpkt.appendChild(pktacja);
 
-divpkt.appendChild(pktacja)
+const img = document.createElement("img");
+img.style.width = "400px";
+img.style.height = "200px";
 
-async function getData(){
-    const dane = await fetch("https://restcountries.com/v3.1/region/europe")
-    const json = await dane.json()
+const h2 = document.createElement("h2");
+h2.style.fontSize = "250%";
 
-    console.log(json);
-
-    return json
-}
-const img = document.createElement("img")
-let h2 = document.createElement("h2")
-
-let inp = document.createElement("input")
-inp.setAttribute("type", "text")
-inp.setAttribute("placeholder","Stolica po angielsku")
-inp.setAttribute("id","inpu")
+const inp = document.createElement("input");
+inp.setAttribute("type", "text");
+inp.setAttribute("placeholder", "Stolica po angielsku");
+inp.setAttribute("id", "inpu");
 inp.style.width = "400px";
 inp.style.height = "30px";
 inp.style.margin = "auto";
+bdy.appendChild(inp);
 
-let btn = document.createElement("button")
-btn.textContent = "Sprawdź!"
+const btn = document.createElement("button");
+btn.textContent = "Sprawdź!";
 btn.style.width = "400px";
 btn.style.height = "30px";
 btn.style.margin = "auto";
+bdy.appendChild(btn);
 
-async function kraje(){
-    const data = await getData()
-    let rand = Math.floor(Math.random()*53)
-    console.log(data[rand].capital);
-    const capital = data[rand].capital
-        img.setAttribute("src", data[rand].flags.png)
-        img.style.width = "400px";
-        img.style.height = "200px";
-        div.appendChild(img)
+let capital = ""; 
 
-        h2.textContent = data[rand].name.common;
-        h2.style.fontSize = "250%";
-        div.appendChild(h2)
-
-        return capital
-        
+async function getData() {
+    const dane = await fetch("https://restcountries.com/v3.1/region/europe");
+    const json = await dane.json();
+    return json;
 }
-async function a(){
-    const data = await kraje()
-    console.log(data[0]);
-    let stolica = document.getElementById("inpu").value
-    if(stolica == data[0]){
-            
-        punktpop = punktpop+1
-        pop.textContent = "Poprawne: "+punktpop;
-        console.log(punktpop);
+
+async function kraj() {
+    const data = await getData();
+    let rand = Math.floor(Math.random() * data.length);
+
+    if (data[rand].capital) { 
+        if (data[rand].capital[0]) { 
+            capital = data[rand].capital[0].toLowerCase(); 
+        }
     }
-    else{
-        punktnie = punktnie+1
-        niepop.textContent = "Niepoprawne: "+punktnie;
+    
+    div.innerHTML = "";  
+    img.setAttribute("src", data[rand].flags.png);
+    div.appendChild(img);
+
+    h2.textContent = data[rand].name.common;
+    div.appendChild(h2);
+}
+
+function odp() {
+    let stolica = inp.value.trim().toLowerCase(); 
+    
+    if (stolica == capital) {
+        punktpop += 1;
+        pop.textContent = "Poprawne: " + punktpop;
+    } else {
+        punktnie += 1;
+        niepop.textContent = "Niepoprawne: " + punktnie;
     }
-    if(punktnie==5)
-    {
-        let koniec = document.createElement("h1")
-        let reset = document.createElement("button")
+
+    inp.value = "";  
+
+    if (punktnie == 5) {
+        const koniec = document.createElement("h1");
+        koniec.textContent = "Przegrałeś! Uzyskałeś 5 niepoprawnych odpowiedzi.";
+        
+        const reset = document.createElement("button");
         reset.textContent = "Zacznij od nowa";
         reset.style.width = "400px";
         reset.style.height = "30px";
         reset.style.margin = "auto";
-        reset.setAttribute("onclick","window.location.reload()")
-        koniec.textContent = "Przegrałeś! Uzyskałeś 5 niepoprawnych odpowiedzi";
-        bdy.appendChild(koniec)
-        bdy.appendChild(reset)
+        reset.setAttribute("onclick", "window.location.reload()");
+        bdy.appendChild(koniec);
+        bdy.appendChild(reset);
 
+        btn.disabled = true;  
+    } else {
+        kraj();  
     }
-
-    
 }
 
-        bdy.appendChild(inp)
-
-
-
-        btn.addEventListener("click",function (){
-            kraje()
-            a()
-        })
-        
-        
-        bdy.appendChild(btn)
-        pktacja.appendChild(pop)
-        pktacja.appendChild(niepop)
-        bdy.appendChild(divpkt)
-        
-
-
-
-kraje()
-
-
-
-
-
-bdy.appendChild(h1)
-bdy.appendChild(div)
+btn.addEventListener("click", odp);
+kraj();
